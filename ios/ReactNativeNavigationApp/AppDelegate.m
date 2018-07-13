17be91d03927773b40eb9f47f05491870b186ab5
Zach Nolan <zach@uplinkconsulting.com>
2018-07-12 20:53:28 -0700

    react-native-navigation iOS setup

--- ios/ReactNativeNavigationApp/AppDelegate.m
+++ ios/ReactNativeNavigationApp/AppDelegate.m
@@ -9,6 +9,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <ReactNativeNavigation/ReactNativeNavigation.h>
 
 @implementation AppDelegate
 
@@ -17,6 +18,8 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  
+  [ReactNativeNavigation bootstrap:jsCodeLocation launchOptions:launchOptions];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ReactNativeNavigationApp"
